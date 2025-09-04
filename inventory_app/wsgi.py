e438818efb1340d1ea7b6d25312c3741196597bf
Scout064 <78973583+Scout064@users.noreply.github.com>
2025-09-04 12:44:06 +0200

    Update wsgi.py
    fixed venv issue

--- inventory_app/wsgi.py
+++ inventory_app/wsgi.py
@@ -1,3 +1,11 @@
-import sys, os
+import sys
+import site  # <--- This is required
+
+# Add project path
 sys.path.insert(0, '/var/www/inventory')
+
+# Add virtual environment site-packages
+site.addsitedir('/var/www/inventory/venv/lib/python3.11/site-packages')
+
+# Import your Flask app
 from app import app as application
