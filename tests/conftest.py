7c514ecb841fd4086b4747e81a927a875857938a
Scout064 <78973583+Scout064@users.noreply.github.com>
2026-03-02 16:40:11 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -39,3 +39,13 @@ def mock_db():
         mock_conn = MagicMock()
         mocked_get_db.return_value = mock_conn
         yield mock_conn
+
+
+@pytest.fixture
+def app():
+    flask_app.config.update({
+        "TESTING": True,
+        "SECRET_KEY": "test_secret",
+        "WTF_CSRF_ENABLED": False  # <--- Add this line
+    })
+    yield flask_app
