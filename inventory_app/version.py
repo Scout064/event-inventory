1752efc15ed16e88d4eaa8ddb4a159359e8797ad
Scout064 <78973583+Scout064@users.noreply.github.com>
2026-03-20 00:39:55 +0100

    Update version.py

--- inventory_app/version.py
+++ inventory_app/version.py
@@ -54,14 +54,6 @@ def get_beta_releases(limit=5):
 
 
 def get_current_version():
-#    version_info = []
     with open(VERSION_PATH, "r", encoding="utf-8") as f:
         data = json.load(f)
-#        for r in data:
-#            version_info.append({
-#                "version": r["version"],
-#                "buildDate": r["buildDate"],
-#                "branch": r["branch"]
-#            })
-#    return version_info
     return data["version"]
